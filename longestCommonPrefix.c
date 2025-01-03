7a1b17c3ae28c4c64c93a7f31b99870e03a0fc21
gzra50 <yousseftag50@gmail.com>
2025-01-03 16:55:34 +0200

    added recent questions

--- longestCommonPrefix.c
+++ longestCommonPrefix.c
@@ -1,44 +1,28 @@
 char* longestCommonPrefix(char** strs, int strsSize)
 {
-    // make the first word our refrence
-    char* refrence = strs[0];
-    char* ret = malloc(200 * sizeof(char));
-    int lastIndex = 0;
-    int flag = 0;
-    int i = 0;
     int j = 0;
+    
+    if (strsSize == 0 || strlen(strs[0]) == 0)
+        return strdup("");
 
-    while (refrence[i] != '\0')
+    // Reference the first string for comparison
+    char* reference = strs[0];
+    int i = 0;
+
+    while (reference[i] != '\0') 
     {
-        while( j < strsSize)
+        while ( j < strsSize)
         {
-            // if the first char doesnt match return emptya array
-            if(strs[j][i] != refrence[i] && i == 0)
-            {
-                strdup("");
+            if (strs[j][i] != reference[i] || strs[j][i] == '\0') {
+                // Terminate the prefix at this point
+                reference[i] = '\0';
+                return reference;
             }
-            else if (strs[j][i] != refrence[i])
-            {
-                flag = 1;
-                break;
-            }
-            // if the char matches the char from the refrence move to the next string
-            if(strs[j][i] == refrence[i])
-            {
-                j++;
-            }
+            j++;
         }
-        if(flag)
-        {
-            break;
-        }
-        if( j == strsSize)
-        {
-            ret[lastIndex] = refrence[i];
-            lastIndex++;
-            i++;
-            j = 0;
-        }      
+         j = 0;
+         i++;   
     }
-        return ret;
+    return reference;    
 }
+
