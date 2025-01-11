9d9beffbc163d7bead442c84236a86623f6078bf
YoussefTagElnasr <yousseftag50@gmail.com>
2025-01-11 23:38:39 +0200

    added recent questions

---
+++ maximumProduct.c
@@ -0,0 +1,15 @@
+
+
+int max( int num1 , int num2)
+{
+    if( num1 > num2)
+        return num1;
+    else
+        return num2;
+}
+
+
+int maximumProduct(int* nums, int numsSize)
+{
+    for(int i = 0; i < numsSizs)
+}
