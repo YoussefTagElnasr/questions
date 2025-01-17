1c0dfce074303602cfd97705a521979cb016c5a2
gzra50 <yousseftag50@gmail.com>
2025-01-17 20:52:29 +0200

    edited file

--- maximumProduct.c
+++ maximumProduct.c
@@ -1,15 +1,65 @@
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
 
+    int leftArr[n1], rightArr[n2];
 
-int max( int num1 , int num2)
-{
-    if( num1 > num2)
-        return num1;
-    else
-        return num2;
+    for (i = 0; i < n1; i++)
+        leftArr[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        rightArr[j] = arr[mid + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
+            i++;
+        }
+        else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
 }
 
 
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+      
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
 int maximumProduct(int* nums, int numsSize)
 {
-    for(int i = 0; i < numsSizs)
+    mergeSort(nums , 0 , numsSize - 1);
+
+    int ret = nums[numsSize-1] * nums[numsSize-2] * nums[numsSize-3];
+    int ret2 = nums[0] * nums[1] * nums[numsSize-1];
+
+    if(ret > ret2)
+        return ret;
+    else
+        return ret2;
 }
