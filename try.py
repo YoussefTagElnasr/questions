beaf57898924c47f09645c4dd8d843f8fafbbc6b
gzra50 <yousseftag50@gmail.com>
2025-02-07 03:11:10 +0200

    added recent questions

---
+++ try.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        counter = 0
+        for i in range(len(s) - 1 , 0 , -1):
+            if(s[i].isalpha()):
+                counter += 1
+            elif(s[i] == " " and (counter != 0)):
+                break
+        return counter
+
+ 
+
