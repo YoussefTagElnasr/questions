c8fc3ae8df42fd70643b7759776aa9ef36adb888
gzra50 <yousseftag50@gmail.com>
2025-02-15 03:00:02 +0200

    added recent question

---
+++ merge.py
@@ -0,0 +1,27 @@
+from typing import List
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> List[int]:
+        i, j, k = m - 1, n - 1, m + n - 1  
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+
+                
+            
+
+
+
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,5,6]
+
+print(Solution().merge(nums1 , 3 , nums2 , 3))
