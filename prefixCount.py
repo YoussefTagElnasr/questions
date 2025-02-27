dd9c1de588c51216e2014a4362875f87e329622c
YoussefTagElnasr <yousseftag50@gmail.com>
2025-02-27 22:24:23 +0200

    added recet question

---
+++ prefixCount.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        count = 0
+        for string in words:
+            if string.startswith(pref):
+                count += 1
+        return count
